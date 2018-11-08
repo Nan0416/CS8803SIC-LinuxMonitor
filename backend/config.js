c5892568d366e3cd05cdd7e46ebaee4373bd42ae
Nan Qin <qinnan0416@gmail.com>
2018-11-08 17:02:33 -0500

    update back

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,11 @@
 const username = "qinnan";
-const target_name = "home";
+const target_name = "Fake";
+const password = "Hello12345";
 const ip = "127.0.0.1";
 const port = 9000;
 
 module.exports.username = username;
+module.exports.target_name = target_name;
+module.exports.password = password;
 module.exports.ip = ip;
 module.exports.port = port;
