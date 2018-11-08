2b81df572bf6f4f331b37eefdbf3aaf1990361c0
Nan Qin <qinnan0416@gmail.com>
2018-11-08 15:04:08 -0500

    adjust code structure

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+const username = "qinnan";
+const password = "QN1994qn";
+const ip = "home.sousys.com";
+const port = 9000;
+
+module.exports.username = username;
+module.exports.password = password;
+module.exports.ip = ip;
+module.exports.port = 9000;
---
+++ backend/index.js
@@ -0,0 +1,74 @@
+/**
+ * Backend RESTful API based on HTTP
+ *  */
+const express = require('express');
+const app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+const serverIP = require('./config').ip;
+const serverPort = require('./config').port;
+const username = require('./config').username;
+const password = require('./config').password;
+const mongodb_url = require('./config').mongodb_url;
+const url_prefix = "/restapi";
+
+
+// query
+const queryCPU = require('./http_routes/query_routes/cpu_route');
+const queryMemory = require('./http_routes/query_routes/memory_route');
+const querySystemInfo = require('./http_routes/query_routes/systeminfo_route');
+const queryNetworkIO = require('./http_routes/query_routes/network_io_route');
+const queryDiskIO = require('./http_routes/query_routes/disk_io_route');
+// test
+const testLatency = require('./http_routes/test_routes/ping_route');
+
+///////// Express configuration //////////
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    limit: '50mb',
+    extended: true
+}));
+
+
+
+app.use(url_prefix + '/query/cpu', queryCPU);
+app.use(url_prefix + '/query/memory', queryMemory);
+app.use(url_prefix + '/query/systeminfo', querySystemInfo);
+app.use(url_prefix + '/query/networkio', queryNetworkIO);
+app.use(url_prefix + '/query/diskio', queryDiskIO);
+// test
+app.use(url_prefix + '/test/latency', testLatency);
+
+// unrecognized path
+app.use(function(req, res, next) {
+    res.statusCode = 400;
+    res.json({
+        success: false,
+        reasons:[`Invalid url path ${req.originalUrl}`],
+        value:null
+    });
+});
+  
+io.on('connection', (socket)=>{
+    if(false){
+        // verification goes here
+        return;
+    }
+    socket.on('disconnect', (msg)=>{
+        deleteSocket(username, socket);
+    });
+    socket.on('subscribe', (msg)=>{
+        addSocket(username, socket);
+    });
+    socket.on("unsubscribe", (msg)=>{
+
+    });
+});
+
+app.listen(monitor_port, monitor_ip);
+console.log(`monitor server is running at http://${ip}:${port}`);
